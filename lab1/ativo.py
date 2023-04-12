2b3426587282cb9ebb677a46fc9d2456b8818014
Carlos Bravo <carlosh.serrado@gmail.com>
2023-04-11 22:29:23 -0300

    Added some comments

--- lab1/ativo.py
+++ lab1/ativo.py
@@ -1,17 +1,28 @@
 import socket
 
+# Dados de conexão
 HOST = 'localhost'
 PORTA = 5000
 
+# Cria o socket
 sock = socket.socket()
 sock.connect((HOST, PORTA))
 
 while True:
+    # Le a mensagem
     msg = input()
+
+    # Verifica se é para terminar
     if msg == "fim":
         break
+
+    # Envia a mensagem
     sock.send(bytes(msg,  encoding='utf-8'))
+
+    # Recebe a mensagem do passivo
     msg = sock.recv(1024)
+
     print('Recebido: ' + str(msg,  encoding='utf-8'))
 
+# Fecha o socket
 sock.close()
--- lab1/passivo.py
+++ lab1/passivo.py
@@ -1,22 +1,36 @@
 import socket
 
+# Dados de conexão
 HOST = ''
 PORTA = 5000
 
+# Cria o socket
 sock = socket.socket()
+
+# Associa o socket a porta
 sock.bind((HOST, PORTA))
+
+# Aguarda conexões
 sock.listen(5)
 print("Pronto para receber conexões...")
 
+# Aceita a conexão
 novoSock, endereco = sock.accept()
 print('Conectado com: ', endereco)
 
 while True:
+    # Recebe a mensagem do ativo
     msg = novoSock.recv(1024)
+
+    # Verifica se a conexão foi encerrada
     if not msg:
         break
+
+    # Envia a mensagem de volta
+    # Não é necessário converter para bytes pois já veio no formato
     print('Mensagem recebida')
     novoSock.send(msg)
 
+# Fecha os sockets
 novoSock.close()
 sock.close()
